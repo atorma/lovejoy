0b52bf92e9c5775eaa78f1a20c9808d95254fda7
atorma <anssi.torma@iki.fi>
2017-03-30 07:30:58 +0300

    Move files

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,13 @@
 
 const webpack = require('webpack');
 const path = require('path');
-const crypto = require('./src/encryption');
+const crypto = require('./src/problem-generation/encryption');
 
 const cleartext = 'Lennä Barceolonaan';
 const encrypted = crypto.encrypt(cleartext);
 
 module.exports = {
-  entry: './src/lovejoy.js',
+  entry: './src/app/lovejoy.js',
   output: {
     filename: 'lovejoy.js',
     path: path.join(__dirname, 'dist')
